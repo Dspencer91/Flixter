0b210cba25033f875cdb5903d471f7cf146e8e76
dillon spencer <dillonstreetfrontcommercial@gmail.com>
2019-11-15 16:11:46 +0000

    Flixter Lesson 11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  https://flixter-dillon-spencer.herokuapp.com/
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

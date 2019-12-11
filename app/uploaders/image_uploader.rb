9c5201ea4850fd2cca38d7e8d6d7fceaad63109e
dillon spencer <dillonstreetfrontcommercial@gmail.com>
2019-12-11 19:03:21 +0000

    lesson 24

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,15 +2,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  #storage :file
+   storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  process resize_to_fill: [800, 350]
+  
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -44,4 +44,3 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-end

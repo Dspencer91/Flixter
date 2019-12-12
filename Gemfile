df422a4a2d3a4a63fe2c697018da16594f5dd53a
dillon spencer <dillonstreetfrontcommercial@gmail.com>
2019-12-12 07:36:56 +0000

    Lesson 24, issues with courses/show.html.erb

--- Gemfile
+++ Gemfile
@@ -71,6 +71,7 @@ end
 
 gem 'simple_form'
 gem 'devise'
-gem 'carrierwave', '~> 2.0'
+gem 'carrierwave', '~> 1.3.1'
 gem "mini_magick"
-gem 'carrierwave-aws'
+gem 'carrierwave-aws', '~> 1.0.2'
+gem 'figaro'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,13 +5,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
   #storage :file
-   storage :aws
+  storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  process resize_to_fill: [800, 350]
-  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
@@ -22,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_fill: [800, 350]
   #
   # def scale(width, height)
   #   # do something
@@ -44,3 +45,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+end
